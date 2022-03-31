bceddfb6b0b1875bc88bbf0aab0e1a55a1183661
Ihar Rubanik <igor.rubanik@gmail.com>
2022-03-31 14:47:13 +0400

    фывфыв

--- header.php
+++ header.php
@@ -10,18 +10,39 @@
 <link rel="icon" type="img/favicon/image/png" sizes="16x16" href="<?php bloginfo('template_url'); ?>/assets/img/favicon//favicon-16x16.png">
     <title><?php bloginfo('description' ); ?></title>
 
-
-
-
-    
-    <noscript>
-        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1425426281239758&ev=PageView&noscript=1"/>
-    </noscript>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2NWKXPNZ38"></script>
-
         <?php wp_head(); ?>
 </head>
 <body>
+    <!-- Messenger Плагин чата Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Плагин чата code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "109574278342460");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v13.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/ru_RU/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
         <div id="page-preloader" class="preloader">
             <div class="preloader__block">
                 <div class="preloader__logo">
