8290da0f533100e21c0f83ba1ada42f0799c591e
Ihar Rubanik <igor.rubanik@gmail.com>
2022-04-07 15:51:31 +0400

    ewew

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
   gtag('config', 'G-2NWKXPNZ38');
 </script>
 
-        <!-- Meta Pixel Code -->
+<!-- Meta Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -32,11 +32,11 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1425426281239758');
+fbq('init', '1017895485745054');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1425426281239758&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=1017895485745054&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
 </head>
