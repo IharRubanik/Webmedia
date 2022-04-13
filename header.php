72cf3eeef65e373d6e0192c47773edaca3214a33
IharRubanik <igor.rubanik@gmail.com>
2022-04-13 15:30:22 +0400

    del

--- header.php
+++ header.php
@@ -38,42 +38,9 @@ fbq('track', 'PageView');
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=1017895485745054&ev=PageView&noscript=1"
 /></noscript>
-<!-- End Meta Pixel Code -->
+
 </head>
 <body>
-
-<!-- Messenger Плагин чата Code -->
-<div id="fb-root"></div>
-
-<!-- Your Плагин чата code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-
-<script>
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute("page_id", "109574278342460");
-  chatbox.setAttribute("attribution", "biz_inbox");
-</script>
-
-<!-- Your SDK code -->
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v13.0'
-    });
-  };
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/ru_RU/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
-
-
         <div id="page-preloader" class="preloader">
             <div class="preloader__block">
                 <div class="preloader__logo">
